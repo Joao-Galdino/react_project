c8566826ef29b88f9d940abd1253222ccf882d76
João Galdino <joao.galdino@acqio.com.br>
2022-11-19 18:42:03 -0300

    feat: Adding light theme

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from '@mui/material'
 import { Routes, Route, Navigate } from "react-router-dom"
 
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<h1> Agasalha Negão</h1>}/>
+            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary'> Botão Agasalhador</Button>}/>
             <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
         </Routes>
     )
